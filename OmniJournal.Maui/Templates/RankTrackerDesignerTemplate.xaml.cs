943d763dffa118576ff81ea2a4fd90835018977c
mjlar <mjlar@DESKTOP-3EUVC8I>
2025-02-11 18:42:19 -0600

    still working

---
+++ OmniJournal.Maui/Templates/RankTrackerDesignerTemplate.xaml.cs
@@ -0,0 +1,9 @@
+namespace OmniJournal.Maui.Templates;
+
+public partial class RankTrackerDesignerTemplate : ContentView
+{
+	public RankTrackerDesignerTemplate()
+	{
+		InitializeComponent();
+	}
+}
